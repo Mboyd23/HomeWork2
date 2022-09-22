fbf777eea63105896c9a3221dcca1bb75620517b
Mboyd23 <70333594+Mboyd23@users.noreply.github.com>
2022-09-22 11:32:21 -0400

    Update question3_Nonrepeated.cpp

--- question3_Nonrepeated.cpp
+++ question3_Nonrepeated.cpp
@@ -2,3 +2,30 @@
 in the given String, for example, if the given String is "aggregate" 
 then the first non-repeated character is "r". Your program will return 
 “first non-repeated character is "r". (10 pts)*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string given;
+
+    cin >> given;
+    
+    int count[256] = {0};
+
+    while(*p)
+        ++count[*p++];
+
+    p = given;
+
+    while(*p){
+        if(1 == count[*p])
+            cout << "The first non-repeated character is " << *p << endl;
+
+    p++;
+    }  
+
+    return 0; 
+}
