250940542cbd2ce77f5bedd6480e8e151bb562a1
Mboyd23 <70333594+Mboyd23@users.noreply.github.com>
2022-09-22 11:16:29 -0400

    Update question1_reverse.cpp

--- question1_reverse.cpp
+++ question1_reverse.cpp
@@ -10,9 +10,27 @@ array is car, then your new array will contain the word rac. (10 pts) */
 
 using namespace std;
 
+void reverseString(string words[], int a, int b){
+    while(a < b){
+        a = 0;
+        b = words.size() - 1;
+        int temp = words[a];
+        words[a++] = words[b];
+        words[b--] = temp;
+    }
+}
+
 int main()
 {
  // use the following array of string for your program.
  string words[6] = {"object","oriented","programming","sciences","dedication","commitment"};
 
+    int soa = sizeof(words) / sizeof(words[0]);
+
+    reverseString(string words[], int a, int b);
+
+    cout << "Reversed String Array: " << endl;
+
+    for(int i=0; i < soa; i++)
+        cout << words[i] << endl;
 }
