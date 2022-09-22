3a8ffac6aca1f377bc5411c50cfcfed64fe0ad4d
Mboyd23 <70333594+Mboyd23@users.noreply.github.com>
2022-09-22 11:33:08 -0400

    Update question2_palindrome.cpp

--- question2_palindrome.cpp
+++ question2_palindrome.cpp
@@ -4,3 +4,30 @@ characters which reads the same backward as forward. For example, the
 given words are radar, level, rotor, kayak palindromes. Your program 
 will return, for example “Yes, the radar is a palindrome” or “No, 
 the word house is not a palindrome”. (10 pts) */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPalindrome(string text){
+    for(int i = 0; i < text.length()/2; i++)
+        if(text[i] != text[text.length() - i - 1])
+            return false;
+
+        return true;
+}
+
+int main()
+{
+    string given;
+
+    cin >> given;
+ 
+    if (isPalindrome(given))
+        cout << "Yes, " << given << " is a palindrome" << endl;
+        else
+        cout << "No, " << given << " is not a palindrome" << endl;";
+ 
+    return 0;
+}
